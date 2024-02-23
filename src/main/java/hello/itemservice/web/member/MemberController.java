abcfd2590cc68b90387900e14ba59289a6b5c79a
YunJiW <dtroy371@gmail.com>
2024-02-23 14:57:20 +0900

    ADD : memberController

---
+++ src/main/java/hello/itemservice/web/member/MemberController.java
@@ -0,0 +1,32 @@
+package hello.itemservice.web.member;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+
+    @GetMapping("/add")
+    public String save(@Valid @ModelAttribute("member") Member member,
+                       BindingResult result){
+
+        if(result.hasErrors()){
+            return "members/addMemberForm";
+        }
+
+        memberRepository.save(member);
+        return "redirect:/";
+    }
+}
