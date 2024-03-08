c09c882a6c83c8ca8bbc1fcce5b4edef3c87b4e1
YunJiW <dtroy371@gmail.com>
2024-03-08 18:21:44 +0900

    feat : argumentResolver 추가

--- src/main/java/hello/itemservice/WebConfig.java
+++ src/main/java/hello/itemservice/WebConfig.java
@@ -1,5 +1,6 @@
 package hello.itemservice;
 
+import hello.itemservice.web.argumentresolver.LoginMemberArgumentResolver;
 import hello.itemservice.web.filter.LogFilter;
 import hello.itemservice.web.filter.LoginCheckFilter;
 import hello.itemservice.web.interceptor.LogInterceptor;
@@ -8,31 +9,34 @@ import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
 //    @Bean
-//    public FilterRegistrationBean logFilter(){
-//        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
-//        filterFilterRegistrationBean.setFilter(new LogFilter());
-//        filterFilterRegistrationBean.setOrder(1);
-//        filterFilterRegistrationBean.addUrlPatterns("/*");
-//
-//        return filterFilterRegistrationBean;
-//    }
+    public FilterRegistrationBean logFilter(){
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+
+        return filterFilterRegistrationBean;
+    }
 
 //    @Bean
-//    public FilterRegistrationBean loginCheckFilter(){
-//        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-//
-//        filterRegistrationBean.setFilter(new LoginCheckFilter());
-//        filterRegistrationBean.setOrder(2);
-//        filterRegistrationBean.addUrlPatterns("/*");
-//        return filterRegistrationBean;
-//    }
+    public FilterRegistrationBean loginCheckFilter(){
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(2);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -46,5 +50,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 .excludePathPatterns("/","/members/add","/login","/logout",
                         "/css/**","/*.ico","/error");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
     }
 }
